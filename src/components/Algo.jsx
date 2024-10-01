ac335013bcfbee985f51276e817a1a57fe68e92a
artherx <xxartherxx@gmail.com>
2024-10-01 09:55:24 -0500

    añadiendo 3d

--- src/components/Algo.jsx
+++ src/components/Algo.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+import Spline from "@splinetool/react-spline/next";
 
 export default function Algo() {
-    const textoMovibleRef = useRef(null); // Usamos ref en lugar de getElementById
+  const textoMovibleRef = useRef(null); // Usamos ref en lugar de getElementById
 
-    useEffect(() => {
-        const textoMovible = textoMovibleRef.current;
+  useEffect(() => {
+    const textoMovible = textoMovibleRef.current;
 
-        const handleMouseMove = (event) => {
-            textoMovible.style.position = 'absolute';
-            textoMovible.style.left = `${event.clientX}px`;  // Usamos left y top para evitar issues con translate
-            textoMovible.style.top = `${event.clientY}px`;
-        };
+    const handleMouseMove = (event) => {
+      textoMovible.style.position = "absolute";
+      textoMovible.style.left = `${event.clientX}px`; // Usamos left y top para evitar issues con translate
+      textoMovible.style.top = `${event.clientY}px`;
+    };
 
-        document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
 
-        // Cleanup para evitar memory leaks
-        return () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, []);
+    // Cleanup para evitar memory leaks
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
-    return <div ref={textoMovibleRef} style={{ position: 'absolute' }}>Este texto seguirá al mouse</div>;
+  return (
+    <div ref={textoMovibleRef} style={{ position: "absolute" }}>
+      Este texto seguirá al mouse
+      
+    </div>
+  );
 }
---
+++ src/components/Title3D.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useRef } from "react";
+import Spline from "@splinetool/react-spline";
+
+export default function Title3D() {
+  return (
+    <div >
+      <Spline  scene="https://prod.spline.design/M9zNjnzZontqdNL5/scene.splinecode" />
+    </div>
+  );
+}
